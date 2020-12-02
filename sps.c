cb7cfa681785182b89117f6891476cd7dacc7f16
KrKOo <krkoo.123@gmail.com>
2020-12-02 03:39:29 +0100

    Load file to memory

--- sps.c
+++ sps.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
 #define ROW_ALLOC_SIZE 5
 #define CELL_ALLOC_SIZE 5
@@ -23,76 +24,111 @@ typedef struct {
     int allocated;
 } Table;
 
+int cell_ctor(Cell *cell)
+{
+    cell->value = NULL;
+    cell->length = 0;
+    cell->allocated = 0;
+
+    return 0;
+}
+
+int cell_dtor(Cell *cell)
+{
+    free(cell->value);
+    cell->length = 0;
+    cell->allocated = 0;
+
+    return 0;
+}
+
+int row_ctor(Row *row)
+{
+    row->cells = NULL;
+    row->length = 0;
+    row->allocated = 0;
+
+    return 0;
+}
+
+int row_dtor(Row *row)
+{
+    free(row->cells);
+    row->length = 0;
+    row->allocated = 0;
+
+    return 0;
+}
+
 int table_ctor(Table *table)
 {
-    table->rows = malloc(sizeof(Row));
+
+    table->rows = NULL;    
+    table->allocated = 0;
     table->length = 0;
-    table->allocated = 1;
-
-    table->rows->cells = malloc(sizeof(Cell));
-    table->rows->length = 0;
-    table->rows->allocated = 1;
-
-    table->rows->cells->value = NULL;
-    table->rows->cells->length = 0;
-    table->rows->cells->allocated = 0;
 
     return 0;
 }
 
-int addRows(Table *table, int n)
-{
-    table->rows = realloc(table->rows, (table->allocated + n) * sizeof(Row));
-    table->allocated += n;
-
-    return 0;
-}
-
-int removeRows(Table *table, int n)
-{
-    if(table->allocated - n > 0)
-    {
-        table->rows = realloc(table->rows, (table->allocated - n) * sizeof(Row));
-        table->allocated -= n;
-    }
-
-    return 0;
-}
-
-int addCells(Row *row, int n)
-{
-    row->cells = realloc(row->cells, (row->allocated + n) * sizeof(Row));
-    row->allocated += n;
-
-    return 0;
-}
-
-int removeCells(Row *row, size_t n)
-{
-    if(row->allocated - n > 0)
-    {
-        row->cells = realloc(row->cells, (row->allocated - n) * sizeof(Row));
-        row->allocated -= n;
-    }
-    else
-    {
-        //ERROR
-    }   
-
-    return 0;
-}
-
-int freeTable(Table *table)
+int table_dtor(Table *table)
 {
     for(int i = 0; i < table->allocated; i++)
     {
-        for(int j = 0; j < table->rows->allocated; j++)
+        for(int j = 0; j < table->rows[i].allocated; j++)
         {
             free(table->rows[i].cells[j].value);
         }
         free(table->rows[i].cells);
-        free(table->rows);
     }
+    free(table->rows);
+    return 0;
+}
+
+int addEmptyRow(Table *table)
+{
+    table->rows = realloc(table->rows, (table->allocated + 1) * sizeof(Row));
+    table->allocated++;    
+    row_ctor(&table->rows[table->length]);
+    table->length++;
+    return 0;
+}
+
+int allocateCellsToRow(Row *row)
+{
+    row->cells = realloc(row->cells, (row->allocated + CELL_ALLOC_SIZE) * sizeof(Cell));
+    row->allocated += CELL_ALLOC_SIZE;
+
+    return 0;
+}
+
+int addCellToRow(Row *row, char *value)
+{
+    if(row->length == row->allocated)
+        allocateCellsToRow(row);
+
+    int valueLength = strlen(value);
+
+    row->cells[row->length].value = realloc(row->cells[row->length].value, (valueLength + 1) * sizeof(char));
+    row->cells[row->length].length = valueLength + 1;
+    row->cells[row->length].allocated = valueLength + 1;
+
+    strcpy(row->cells[row->length].value, value);
+    row->length++;
+
+    return 0;
+}
+
+int appendCharToCell(Cell *cell, char c)
+{
+    if(cell->length == cell->allocated)
+    {
+        cell->value = realloc(cell->value, (cell->allocated + CELL_ALLOC_SIZE) * sizeof(char));
+        cell->allocated += CELL_ALLOC_SIZE;
+    }
+    
+    cell->value[cell->length] = c;
+    cell->length++;
+
     return 0;
 }
 
@@ -106,6 +142,17 @@ int isValidDelim(char *delim)
     return 1;
 }
 
+bool isCharInString(char c, char *str)
+{
+    for(int i = 0; str[i] != '\0'; i++)
+    {
+        if(c == str[i])
+            return true;
+    }
+
+    return false;
+}
+
 int main(int argc, char **argv)
 {
     (void)argc;
@@ -115,7 +162,7 @@ int main(int argc, char **argv)
 
     char *delim = " ";
     if (argc > 2)
-        if (strcmp(argv[1], "-d"))
+        if (strcmp(argv[1], "-d") == 0)
             delim = argv[2];
 
     if (isValidDelim(delim) == 0)
@@ -125,16 +172,56 @@ int main(int argc, char **argv)
     }
 
     Table table;
+
     table_ctor(&table);
 
-    freeTable(&table);
+    Cell cell;
+    cell_ctor(&cell);
 
     FILE *inputFile = fopen("tab.txt", "r");
     char c;
+    int rowID = 0;
+    int colID = 0;
+    addEmptyRow(&table);
     while ((c = fgetc(inputFile)) != EOF)
     {
-        printf("%c", c);
+        
+        if(isCharInString(c, delim))
+        {
+            appendCharToCell(&cell, '\0');
+            addCellToRow(&table.rows[rowID], cell.value);
+            cell_dtor(&cell);
+            cell_ctor(&cell);
+            colID++;
+        }
+        else if(c == '\n')
+        {
+            appendCharToCell(&cell, '\0');
+            addCellToRow(&table.rows[rowID], cell.value);
+            cell_dtor(&cell);
+            cell_ctor(&cell);
+            addEmptyRow(&table);
+            rowID++;
+            colID=0;
+        }
+        else
+        {
+            appendCharToCell(&cell, c);
+        }
     }
+
+    fclose(inputFile);
+
+    for(int i = 0; i < table.length - 1; i++)
+    {
+        for(int j = 0; j < table.rows->length; j++)
+        {
+            printf("%s%c", table.rows[i].cells[j].value, delim[0]);
+        }
+        putchar('\n');
+    }
+
+    table_dtor(&table);
 
     return 0;
 }
